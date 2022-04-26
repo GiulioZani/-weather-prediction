a6e2d09b59d21b97551ab795618c8a8a5d36f0b8
GiulioZani <giulio.zani@gmail.com>
2022-04-26 15:31:02 +0200

    fixed test

--- main/utils/data_manager.py
+++ main/utils/data_manager.py
@@ -7,17 +7,23 @@ class DataManager:
     def __init__(self, *, data: t.Tensor, norm_type="minmax"):
         self.norm_type = norm_type
         flattened_data = data.reshape(-1, 5).float()
-        self.min = flattened_data.min(dim=0)[0][None, None, :]
-        self.max = flattened_data.max(dim=0)[0][None, None, :]
-        self.var = t.var(flattened_data, dim=0)[None, None, :]
-        self.mean = t.mean(flattened_data, dim=0)[None, None, :]
+        device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+        self.min = flattened_data.min(dim=0)[0][None, None, :].to(device)
+        self.max = flattened_data.max(dim=0)[0][None, None, :].to(device)
+        self.var = t.var(flattened_data, dim=0)[None, None, :].to(device)
+        self.mean = t.mean(flattened_data, dim=0)[None, None, :].to(device)
 
     def normalize(self, data: t.Tensor) -> t.Tensor:
+        old_device = self.min.device
+        if data.device != self.min.device:
+            old_device = data.device
+            data = data.to(self.min.device)
         result = data
         if self.norm_type == "minmax":
             result = (data - self.min) / (self.max - self.min)
         elif type == "meanvar":
             result = (data - self.mean) / self.var
+        result = result.to(old_device)
         return result
 
     def denormalize(self, data: t.Tensor) -> t.Tensor:
