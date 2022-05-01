7e437cf29b1cf018234952a85271f0a8b85eb382
GiulioZani <giulio.zani@gmail.com>
2022-05-01 22:30:25 +0200

    fixed metrics

--- main/base_lightning_modules/base_model.py
+++ main/base_lightning_modules/base_model.py
@@ -2,7 +2,7 @@ from pytorch_lightning import LightningModule
 import torch as t
 import torch.nn.functional as F
 from argparse import Namespace
-from ..utils.visualize_predictions import visualize_predictions
+# from ..utils.visualize_predictions import visualize_predictions
 import matplotlib.pyplot as plt
 import ipdb
 import os
@@ -26,7 +26,14 @@ class BaseModel(LightningModule):
 
     def training_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
         x, y = batch
-        y_pred = self(x)
+        context = x
+        for _ in range(self.params.lag):
+            pred = self.generator(context[:, :self.params.in_seq_len])
+            context = t.cat((context, pred), dim=1)
+        y_pred = context[:, -self.params.in_seq_len:]
+        #y_pred = self(x)out_seq_len
+        #y = y[:, :, -1, 2]
+        #y_pred = y_pred[:, :, -1, 2]
         loss = self.loss(y_pred, y)
         return loss
 
@@ -37,11 +44,100 @@ class BaseModel(LightningModule):
     ):
         x, y = batch
         if batch_idx == 0:
-            visualize_predictions(x, y, self(x), path=self.params.save_path)
-        pred_y = self.generator(x, future_step=y.shape[1])[0]
+            # visualize_predictions(x, y, self(x), path=self.params.save_path)
+            pass
+        pred_y = self.generator(x, future_step=y.shape[1])
+        pred_y = pred_y[0] if isinstance(pred_y, tuple) else pred_y
         loss = F.mse_loss(pred_y, y)
         self.log("val_mse", loss, prog_bar=True)
         return {"val_mse": loss}
+
+    
+    
+    def configure_optimizers(self):
+        optimizer = t.optim.Adam(self.parameters(), lr=self.params.lr)
+        scheduler = t.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            factor=0.1,
+            patience=self.params.reduce_lr_on_plateau_patience,
+            min_lr=1e-6,
+            verbose=True,
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": scheduler,
+            "monitor": "val_mse",
+        }
+
+    def plot_test(self):
+        test_dl = self.trainer.test_dataloaders[0]
+        for batch in test_dl:
+            x, y = batch
+
+            pred_y = self.generator(x.to(self.device), future_step=y.shape[1])
+            pred_y = pred_y[0] if isinstance(pred_y, tuple) else pred_y
+
+            y = self.params.data_manager.denormalize(y.cuda()).cpu()
+            pred_y = self.params.data_manager.denormalize(pred_y.cuda()).cpu()
+
+            pred_y_city = pred_y[0, :, -1, 2].cpu()
+            y_city = y[0, :, -1, 2]
+            xs = t.arange(len(pred_y_city))
+            plt.plot(xs, pred_y_city, label="prediction")
+            plt.plot(xs, y_city, label="ground truth")
+            plt.legend()
+            plt.savefig(
+                os.path.join(self.params.save_path, "168_final_prediction.png")
+            )
+            plt.clf()
+            plt.plot(
+                xs[: self.params.test_seq_len],
+                pred_y_city[: self.params.test_seq_len],
+                label="prediction",
+            )
+            plt.plot(
+                xs[: 10],
+                y_city[: 10],
+                label="ground truth",
+            )
+            plt.legend()
+            plt.savefig(
+                os.path.join(self.params.save_path, "10_final_prediction.png")
+            )
+
+
+    def plot_importance_scores(self):
+        importance_scores = self.generator.get_importance_scores()
+        plt.title("Variable Importance Scores")
+        plt.imshow(
+            importance_scores[0], cmap="hot", interpolation="nearest"
+        )
+        plt.savefig(
+            os.path.join(
+                self.params.save_path, "variables_importance_scores.png"
+            )
+        )
+        plt.title("City Importance Scores")
+        plt.imshow(
+            importance_scores[1], cmap="hot", interpolation="nearest"
+        )
+        plt.savefig(
+            os.path.join(
+                self.params.save_path, "city_importance_scores.png"
+            )
+        )
+
+
+    def test_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
+        x, y = batch
+
+        if batch_idx == 0:
+            # visualize_predictions(x, y, self(x), path=self.params.save_path)
+            pass
+
+        pred_y = self.generator(x, future_step=y.shape[1])
+        return self.test_without_forward(y, pred_y)
 
     def test_without_forward(self, y, pred_y):
         y_single = y  # [:, :, -1, 2]
@@ -77,95 +173,19 @@ class BaseModel(LightningModule):
             "fn": fn,
             "tp": tp,
             "temp_ae": temp_ae,
-            "total_lengh": total_lengh,
+            "total_length": total_lengh,
+            "temp_length": denorm_y[:, :, -1, 2].numel(),
         }
 
-    def test_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
-        x, y = batch
-
-        if batch_idx == 0:
-            visualize_predictions(x, y, self(x), path=self.params.save_path)
-
-        pred_y = self(x)
-        return self.test_without_forward(y, pred_y)
-
-    def configure_optimizers(self):
-        optimizer = t.optim.Adam(self.parameters(), lr=self.params.lr)
-        scheduler = t.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            mode="min",
-            factor=0.1,
-            patience=self.params.reduce_lr_on_plateau_patience,
-            min_lr=1e-6,
-            verbose=True,
-        )
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": scheduler,
-            "monitor": "val_mse",
-        }
-
-    def plot_test(self):
-        test_dl = self.trainer.test_dataloaders[0]
-        for batch in test_dl:
-            x, y = batch
-            pred_y = self.generator(x.to(self.device), future_step=y.shape[1])[
-                0
-            ]
-            pred_y_city = pred_y[0, :, -1, 2].cpu()
-            y_city = y[0, :, -1, 2]
-            xs = t.arange(len(pred_y_city))
-            plt.plot(xs, pred_y_city, label="prediction")
-            plt.plot(xs, y_city, label="ground truth")
-            plt.legend()
-            plt.savefig(
-                os.path.join(self.params.save_path, "168_final_prediction.png")
-            )
-            plt.clf()
-            plt.plot(
-                xs[: self.params.test_seq_len],
-                pred_y_city[: self.params.test_seq_len],
-                label="prediction",
-            )
-            plt.plot(
-                xs[: self.params.test_seq_len],
-                y_city[: self.params.test_seq_len],
-                label="ground truth",
-            )
-            plt.legend()
-            plt.savefig(
-                os.path.join(self.params.save_path, "10_final_prediction.png")
-            )
-
-    def plot_importance_scores(self):
-        if hasattr(self.generator, "get_importance_scores"):
-            importance_scores = self.generator.get_importance_scores()
-            plt.title("Variable Importance Scores")
-            plt.imshow(
-                importance_scores[0], cmap="hot", interpolation="nearest"
-            )
-            plt.savefig(
-                os.path.join(
-                    self.params.save_path, "variables_importance_scores.png"
-                )
-            )
-            plt.title("City Importance Scores")
-            plt.imshow(
-                importance_scores[1], cmap="hot", interpolation="nearest"
-            )
-            plt.savefig(
-                os.path.join(
-                    self.params.save_path, "city_importance_scores.png"
-                )
-            )
 
     def test_epoch_end(self, outputs):
         self.plot_test()
-        total_lenght = sum([x["total_lengh"] for x in outputs])
+        temp_lenght = sum([x["temp_length"] for x in outputs])
+        total_lenght = sum([x["total_length"] for x in outputs])
         mse = t.stack([x["se"] for x in outputs]).sum() / total_lenght
         mae = t.stack([x["ae"] for x in outputs]).sum() / total_lenght
         mae_temp = (
-            t.stack([x["temp_ae"] for x in outputs]).sum() / total_lenght
+            t.stack([x["temp_ae"] for x in outputs]).sum() / temp_lenght
         )
         tn = t.stack([x["tn"] for x in outputs]).sum() / total_lenght
         fp = t.stack([x["fp"] for x in outputs]).sum() / total_lenght
